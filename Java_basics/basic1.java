0ceeef9dcce9a361f4a9d1e53e4ded8484088953
Adnan9-63 <muhammad1414114@gmail.com>
2026-08-24 17:55:54 +0530

    very basic abcd..

--- Java_basics/basic1.java
+++ Java_basics/basic1.java
@@ -1,5 +1,6 @@
 public class basic1 {
     public static void main(String[] args) {
         System.out.println("Java environment configured successfully.");
+      
     }
 }
---
+++ Java_basics/basic2.java
@@ -0,0 +1,21 @@
+public class basic2 {
+    public static void main(String[] args)  {
+        System.out.print("hello world!!"); //no default next line
+        System.out.println("Same line but next statement on next line");  // default next line
+        System.out.println("Now on the next line");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        // Variables in  java
+        int a=10;
+        int b=20;
+        System.out.println("Sum of a and b is: "+(a+b));
+
+
+
+    }   
+
+    
+}

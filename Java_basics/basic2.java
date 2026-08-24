3466f479f5db475712e4354cdc4d26c25b7241c3
Adnan9-63 <muhammad1414114@gmail.com>
2026-08-24 18:14:10 +0530

    still basic byte during operation converts evry variable into integer  and the final one is also integer.

--- Java_basics/basic2.java
+++ Java_basics/basic2.java
@@ -12,8 +12,36 @@ public class basic2 {
         int a=10;
         int b=20;
         System.out.println("Sum of a and b is: "+(a+b));
+        a=b;
+        System.out.println("Value of a is: "+a);
+        
 
+        /*
+         TWO types of Datatype in java 
 
+            1. Primitive Datatype - byte,short,char,int,long,float,double,boolean   
+            2. Non-Primitive Datatype - string, array, class, interface, object, etc.   
+
+        
+        */  
+         
+         byte d=8;
+         System.out.println("Value of b is: "+d);
+         char ch = 'a'; // default value of char is null
+         System.out.println("Value of ch is: "+ch); // default value of char is null
+         boolean var = false;
+        //  float price=10.5f;
+         //long, double, short
+
+         short n=240;
+        //  System.out.println(ch+n+price);
+        // float res = (float)(ch+n+price);
+        // System.out.println("Result is: "+res);
+        byte x=10;
+        byte y=20;
+        int z=x+y;
+        byte res = (byte)(x+y);
+        System.out.println("Result is: "+res);
 
     }   
 
